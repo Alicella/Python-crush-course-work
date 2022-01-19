5fd94ace0a8b08979a81221fce751760490c4702
Alicella <87024873+Alicella@users.noreply.github.com>
2022-01-19 13:39:31 +0800

    Add files via upload

---
+++ 9-9_classes.py
@@ -0,0 +1,55 @@
+class Car():
+    """A simple attempt to represent a car."""
+
+    def __init__(self, make, model, year):
+        self.make = make
+        self.model = model
+        self.year = year
+
+    def get_descriptive_name(self):
+        long_name = str(self.year) + ' ' + self.make + ' ' + self.model
+        return long_name.title()
+
+
+class Battery():
+    """A simple attempt to model a battery for an electric car."""
+
+    def __init__(self, battery_size=70):
+        """Initialize the battery's attributes."""
+        self.battery_size = battery_size
+
+    def describe_battery(self):
+        """Print a statement describing the battery size."""
+        print("This car has a " + str(self.battery_size) + "-kWh battery.")
+
+    def get_range(self):
+        """Print a statement about the range this battery provides."""
+        if self.battery_size == 70:
+            range = 240
+        elif self.battery_size == 85:
+            range = 270
+
+        message = "This car can go approximately " + \
+            str(range) + " miles on a full charge."
+        print(message)
+
+    def update_battery(self):
+        if self.battery_size != 85:
+            self.battery_size = 85
+
+
+class ElectricCar(Car):
+    """Represent aspects of a car, specific to electric vehicles."""
+
+    def __init__(self, make, model, year):
+        """Initialize attributes of the parent class."""
+        super().__init__(make, model, year)
+        self.battery = Battery()
+
+
+my_tesla = ElectricCar('tesla', 'model s', 2016)
+# print(my_tesla.get_descriptive_name())
+
+my_tesla.battery.update_battery()
+my_tesla.battery.describe_battery()
+my_tesla.battery.get_range()
